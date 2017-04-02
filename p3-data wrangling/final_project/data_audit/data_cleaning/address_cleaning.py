1d1e03c72dd50f0d7b1540ce0d72dbbe8ad4f038
Ravi Akella <ravisankar.akella@gmail.com>
2017-04-02 13:15:36 -0500

    Comments

--- p3-data wrangling/final_project/data_audit/data_cleaning/address_cleaning.py
+++ p3-data wrangling/final_project/data_audit/data_cleaning/address_cleaning.py
@@ -26,10 +26,16 @@ mapping = { "NE": "Northeast",
           }
 
 
+"""
+This method checks if address information is present in the given tag or not
+"""
 def is_street_name(tag):
     return tag.attrib['k'] == "addr:street"
 
 
+"""
+This method audits and cleans the address
+"""
 def audit_and_update_street_name(street_name):
     if " " in street_name:
         name = street_name.split(" ")
@@ -41,6 +47,9 @@ def audit_and_update_street_name(street_name):
     return street_name
 
 
+"""
+This method updates unexpected street name values
+"""
 def update_unexpected_street_names(element):
     street_name = ""
 
@@ -51,25 +60,40 @@ def update_unexpected_street_names(element):
     return street_name
 
 
+"""
+This method checks if post code information is present in the given tag or not
+"""
 def is_contains_post_code(tag):
     return tag.attrib['k'] == "addr:postcode"
 
 
+"""
+This is the initialization method for post code
+"""
 def get_post_codes(element):
     for tag in element.iter("tag"):
         if is_contains_post_code(tag):
             return tag.attrib['v']
 
+
+"""
+This method checks if house number information is provided in the data
+"""
 def is_house_number(tag):
     return tag.attrib['k'] == "addr:housenumber"
 
 
+"""
+This method retrieved the house number information
+"""
 def get_house_number(element):
     for tag in element.iter("tag"):
         if is_house_number(tag):
             return tag.attrib['v']
 
-
+"""
+This method provides the address information to the __init__ class
+"""
 def get_address_information(element):
     address = {}
 
--- p3-data wrangling/final_project/data_audit/data_cleaning/phone_number_cleaning.py
+++ p3-data wrangling/final_project/data_audit/data_cleaning/phone_number_cleaning.py
@@ -2,6 +2,24 @@ import xml.etree.cElementTree as ET
 
 FILE_NAME = "Seattle_dataset.osm"
 
+"""
+This method cleanes the phone numbers.
+
+The different types of out of order phone numbers observed are:
+ 1-206-296-4441
++1-206-441-9729
++1 (844) 585 2487 x3
++ 206 436-6836
+12067200969
++!-206-453-3068
++1 206-622–2036
+206-903-022
++1 206–547–1226
+http://www.couperokei.com
+206-621-8656;2066218656
+
+This method cleanes such data and returns phone numbers in +1 (xxx) yyy-zzzz format.
+"""
 def update_phone_numbers(phone_number):
 
     formatted_phone_number = ""
@@ -73,10 +91,16 @@ def update_phone_numbers(phone_number):
     return formatted_phone_number
 
 
+"""
+This method checks if the given tag contains phone number attribute
+"""
 def has_phone_number(tag):
     return tag.attrib['k'] == "phone"
 
 
+"""
+This is the initialization method to process phone numbers
+"""
 def get_phone_number_valid_format(element):
     updated_phone_number = ""
 
